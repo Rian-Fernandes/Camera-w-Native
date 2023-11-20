9be350cc8b109940d5710eaed19fef29a4eeaa95
Rian-Fernandes <rian10fernandes@hotmail.com>
2023-11-20 12:02:12 -0300

    Criando botões para alternar cameras e tirar foto

--- App.js
+++ App.js
@@ -1,10 +1,15 @@
-import React, { useEffect, useState } from 'react';
-import { StyleSheet, Text, View, SafeAreaView, Alert } from 'react-native';
+import React, { useEffect, useState, useRef } from 'react';
+import { StyleSheet, Text, View, SafeAreaView, Alert, TouchableOpacity } from 'react-native';
 import { Camera } from 'expo-camera';
+import { CameraType } from 'expo-camera';
+
+import {FontAwesome} from "@expo/vector-icons"
 
 export default function App() {
+  const camRef = useRef(null)
   const [type, setType] = useState(Camera.Constants.Type.back);
   const [hasPermission, setHasPermission] = useState(null);
+  const [capturedPhoto, setCapturedPhoto] = useState(null)
 
   useEffect(() => {
     (async () => {
@@ -32,9 +37,36 @@ export default function App() {
     );
   }
 
+  async function takePicture(){
+    if(camRef){
+    const data = await camRef.current.takePictureAsync();
+    setCapturedPhoto(data.uri)
+  }
+  }
+
   return (
     <SafeAreaView style={styles.container}>
-      <Camera type={type} style={styles.camera}></Camera>
+      <Camera type={type} style={styles.camera}>
+        <View style={styles.contentButtons}>
+          <TouchableOpacity style={styles.buttonFlip}
+          onPress={() =>{
+            setType(
+              type === Camera.Constants.Type.back 
+              ? Camera.Constants.Type.front
+              : Camera.Constants.Type.back
+            )
+          }}
+          >
+            <FontAwesome name="exchange" size={23} color="red"></FontAwesome>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.buttonCamera}
+            onPress={takePicture}
+          >
+            <FontAwesome name="camera" size ={23} color="#fff"></FontAwesome>
+          </TouchableOpacity>
+        </View>
+      </Camera>
     </SafeAreaView>
   );
 }
@@ -48,4 +80,36 @@ const styles = StyleSheet.create({
     width: '100%',
     height: '100%',
   },
+
+  contentButtons:{
+    flex:1,
+    backgroundColor: "transparent",
+    flexDirection: "row",
+  },
+
+  buttonFlip:{
+    position: "absolute",
+    bottom: 50,
+    left:30,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#fff",
+    margin: 20,
+    height:50,
+    width:50,
+    borderRadius:50
+  },
+
+  buttonCamera:{
+    position: "absolute",
+    bottom: 50,
+    right:30,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "red",
+    margin: 20,
+    height:50,
+    width:50,
+    borderRadius:50
+  }
 });
